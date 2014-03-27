ad51a2977dc8e94963a00e4c3d43204d21271d6b
Duc Cuong <duccuong5199@gmail.com>
2014-03-27 22:52:29 +0700

    Some basis for rotation of Wheel

--- Sources/WOF/src/wof/model/Wheel.java
+++ Sources/WOF/src/wof/model/Wheel.java
@@ -7,18 +7,24 @@
 
 package wof.model;
 
+import javafx.animation.Interpolator;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
-import javafx.scene.Group;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Node;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
+import javafx.util.Duration;
 
 /**
  * Description: Class for the wheel of application
  * @author duc cuong
  */
 
-public class Wheel extends Group {
+public class Wheel extends StackPane {
 
     /**
      * Angle in degree corresponds to each piece of the wheel
@@ -47,6 +53,12 @@ public class Wheel extends Group {
      * Timeline for the animation of wheel
      */
     private Timeline timeline;
+    
+    // temps
+    private double[] startAngle = {0};
+    private double[] endAngle = {305};
+  
+    private final int cycle = 1;
     /**
      * The parent node that contains this wheel
      */
@@ -73,12 +85,13 @@ public class Wheel extends Group {
     }
     
     public Wheel() {
-        height = 600.0;
-        width = 600.0;
+        height = 700.0;
+        width = 700.0;
         rotateTime = 5;
         decreaseAcc = 2;
         setBackgroundImage();     
         radius = 300;
+        this.setId("wheel");
     }
     
     /**
@@ -113,7 +126,58 @@ public class Wheel extends Group {
 
     public void setRadius(double radius) {
         this.radius = radius;
+    }     
+    
+    public void rotate(double from, double to) {
+        timeline = new Timeline();
+        timeline.setCycleCount(cycle);
+        timeline.setAutoReverse(false);
+        
+        from = startAngle[0];
+        to = endAngle[0];
+        
+        timeline.getKeyFrames().addAll(
+                new KeyFrame(Duration.ZERO, new KeyValue(bgImageView.rotateProperty(), from)),
+                new KeyFrame(Duration.millis(2000), new KeyValue(bgImageView.rotateProperty(),to,Interpolator.EASE_OUT))
+        );
+        //this.getChildren().add(bgImageView);
+        timeline.play();
+        timeline.setOnFinished(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent ae) {
+                startAngle[0] = endAngle[0];
+                endAngle[0] = endAngle[0] * 1.8;
+                if (endAngle[0] > startAngle[0] * 4) {
+                    startAngle[0] = 0;
+                    endAngle[0] = 305;
+                }
+            }
+        });
     }
     
+    @Override
+    public void setHeight(double height) {
+        super.setHeight(height);
+    }
     
+    @Override
+    public void setWidth(double width) {
+        super.setHeight(width);
+    }
+    
+    public double getStartAngle() {
+        return startAngle[0];
+    }
+
+    public void setStartAngle(double startAngle) {
+        this.startAngle[0] = startAngle;
+    }
+
+    public double getEndAngle() {
+        return endAngle[0];
+    }
+
+    public void setEndAngle(double endAngle) {
+        this.endAngle[0] = endAngle;
+    }
 }
